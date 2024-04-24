d54398d7aa5f7bcd38faf21edcd212a673b09738
hideakifukami <denys.fukami@gmail.com>
2024-04-24 18:16:15 -0300

    sidebar

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,8 +1,12 @@
 import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
 function Dashboard() {
   return (
+    <>
     <Navbar></Navbar>
+    <Sidebar></Sidebar>
+    </>
   );
 }
 
