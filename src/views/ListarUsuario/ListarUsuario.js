03bbe39aa8806ac2f6a0ad5c5b4972018cd236dd
joao pedro <jplima.dev@outlook.com>
2024-05-01 18:52:28 -0300

    bug_listar_usuarios

--- src/views/ListarUsuario/ListarUsuario.js
+++ src/views/ListarUsuario/ListarUsuario.js
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import Sidebar from "../../components/Sidebar/Sidebar";
+//import Sidebar from "../../components/Sidebar/Sidebar";
 import axios from "axios";
 
 function ListarUsuario() {
     const [users, setUsers] = useState([]);
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('token');
     const auth = {
       headers: {
         'Authorization': `Bearer ${token}`
       }
     };
-
+    
     useEffect(() => {
-        axios.get("http://localhost:8001/listar-usuarios")
-            .then(response => setUsers(response.data))
-            .catch(err => console.log(err));
-    }, [auth]);
+      axios.get("http://localhost:8001/listar-usuarios", auth)
+        .then(response => setUsers(response.data))
+        .catch(err => console.log(err));
+    }, []); // <- Coloque os parênteses aqui para indicar que é uma dependência vazia
 
     return (
         <div className="dashboard-container">
