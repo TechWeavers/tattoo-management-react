d637d517f9802ba2dd7f19fe3a06629eabb21782
Gabriel Almeida <GabrielAlmeida.dev@outlook.com>
2024-05-01 21:03:50 -0300

    listar

--- src/views/ListarUsuario/ListarUsuario.js
+++ src/views/ListarUsuario/ListarUsuario.js
@@ -1,22 +1,8 @@
 import Navbar from "../../components/Navbar/Navbar";
-//import Sidebar from "../../components/Sidebar/Sidebar";
-import axios from "axios";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import ListarUsuarioTable from "../../components/ListarUsuarioTable/ListarUsuarioTable";
 
 function ListarUsuario() {
-    const [users, setUsers] = useState([]);
-    const token = localStorage.getItem('token');
-    const auth = {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    };
-    
-    useEffect(() => {
-      axios.get("http://localhost:8001/listar-usuarios", auth)
-        .then(response => setUsers(response.data))
-        .catch(err => console.log(err));
-    }, []); // <- Coloque os parênteses aqui para indicar que é uma dependência vazia
-
     return (
         <>
         <Navbar></Navbar>
