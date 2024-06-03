b1e59ecb8e77989f5172868438ad712412d858a6
Gabriel Almeida <GabrielAlmeida.dev@outlook.com>
2024-06-03 15:30:28 -0300

    dashboard osso

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -3,12 +3,72 @@ import Sidebar from "../../components/Dashboard/Sidebar/Sidebar"
 import './Dashboard.css'; 
 
 function Dashboard() {
+
+  
+  
+
+
   return (
     <>
-     <Navbar></Navbar> 
+      <Navbar></Navbar> 
+
+      <div class="container">
+        <div>
+          <div class="container d-flex justify-content-center mt-5">
+            <div class="col-md-6">
+              <div class="card shadow-lg rounded-3">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Cima esquerda</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="card shadow-lg rounded-3">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Cima direita</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+        <div class="container d-flex justify-content-center mt-3">
+          <div class="row">
+            <div class="col-md-4 mb-4">
+              <div class="card shadow-lg rounded-3">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Baixo esquerda</h5>
+                  
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 mb-4">
+              <div class="card shadow-lg rounded-3">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Baixo meio</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 mb-4">
+              <div class="card shadow-lg rounded-3">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Baixo </h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        </div>
+        
+      </div>
+
+      
+
+
       <div className="row">
-     <Sidebar></Sidebar> 
-    </div>
+        <Sidebar></Sidebar> 
+      </div>
     </>
   );
 }
