db51ea4faf601235cbe9796099d0c87e1b1cc876
joao <jplima.dev@outlook.com>
2024-06-05 20:59:21 -0300

    colocando proteçao do token

--- src/views/Agendamento/Calendario/CalendarioView.js
+++ src/views/Agendamento/Calendario/CalendarioView.js
@@ -1,19 +1,31 @@
 import Navbar from "../../../components/Dashboard/Navbar/Navbar";
 import Sidebar from "../../../components/Dashboard/Sidebar/Sidebar";
 import Calendario from "../../../components/Agendamento/Calendario"
+import Error from "../../../components/Error/Error"
 
 function CalendarioView() {
-  return (
-    <>
-    <Navbar></Navbar>
-    
-        <div class="row">
-    <Sidebar></Sidebar>
-    <Calendario></Calendario>
-    </div>
-    </>
-  );
+
+  if(localStorage.getItem('token')){
+    return (
+      <>
+      <Navbar></Navbar>
+      
+          <div class="row">
+      <Sidebar></Sidebar>
+      <Calendario></Calendario>
+      </div>
+      </>
+    );
+  }else{
+    return (
+      <>
+        <Error></Error>
+      </>
+    );
+  }
 }
+ 
+
 
 export default CalendarioView;
 
--- src/views/Agendamento/ListarAgendamento/ListarAgendamento.js
+++ src/views/Agendamento/ListarAgendamento/ListarAgendamento.js
@@ -1,18 +1,29 @@
 import Navbar from "../../../components/Dashboard/Navbar/Navbar";
 import Sidebar from "../../../components/Dashboard/Sidebar/Sidebar";
 import ListarCalendarioTable from "../../../components/Agendamento/ListarCalendarioTable";
+import Error from "../../../components/Error/Error"
 
 function ListarAgendamento() {
-  return (
-    <>
-    <Navbar></Navbar>
-    
-        <div class="row">
-    <Sidebar></Sidebar>
-    <ListarCalendarioTable></ListarCalendarioTable>
-    </div>
-    </>
-  );
+
+  if(localStorage.getItem('token')){
+    return (
+      <>
+      <Navbar></Navbar>
+      
+          <div class="row">
+      <Sidebar></Sidebar>
+      <ListarCalendarioTable></ListarCalendarioTable>
+      </div>
+      </>
+    );
+  }else{
+    return (
+      <>
+        <Error></Error>
+      </>
+    );
+  }
+  
 }
 
 export default ListarAgendamento;
