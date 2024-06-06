db8080ea6755d077fdb1fdda1f2f2c56ba3cfa47
Kauansddsdela <116198015+Kauansddsdela@users.noreply.github.com>
2024-06-05 22:52:18 -0300

    Arrumar error

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -3,18 +3,14 @@ import React from "react";
 function Error() {
     return (
         <div className="d-flex align-items-center justify-content-center vh-100">
-            <div className="col-md-6 col-lg-4 col-xl-3 col-xxl-3">
-                <div className="card mb-5 login shadow-sm">
+            <div className="col-md-6">
+                <div className="card bg-white border  shadow-lg border-3 rounded-3 bg-opacity-25 ">
                     <div className="card-body p-sm-5">
                         <div className="row justify-content-center mb-4">
                             <div className="col-md-6 text-center">
                                 <h2 className="mb-4">Erro 404</h2>
                                 <p className="mb-4">Página não encontrada</p>
                                 <a href="/" className="btn btn-primary fw-normal">Voltar para a página inicial</a>
-                            </div>
-                        </div>
-                        <div className="row justify-content-center">
-                            <div className="col-md-6 text-center">
                             </div>
                         </div>
                     </div>
