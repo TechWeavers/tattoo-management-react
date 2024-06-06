3a834efb56ecc7d78edebb74299737cde36e2905
joao <jplima.dev@outlook.com>
2024-06-05 22:11:28 -0300

    bug rota de usuario

--- src/views/CadastroUsuario/CadastroUsuario.js
+++ src/views/CadastroUsuario/CadastroUsuario.js
@@ -2,20 +2,54 @@ import Navbar from "../../components/Dashboard/Navbar/Navbar";
 import NovoUsuarioForm from "../../components/Cadastro/NovoUsuarioForm/NovoUsuarioForm";
 import Sidebar from "../../components/Dashboard/Sidebar/Sidebar";
 import Error from "../../components/Error/Error"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function CadastroUsuario() {
 
+  const [tipo, setTipo] = useState('')
+
+  useEffect(()=>{
+    const token = localStorage.getItem('token')
+    
+    const carregarAtributo = async () => {
+      try {
+
+          const response = await axios.get(`http://localhost:8000/tipo-usuario/${token}`);
+
+          // Extrair o atributo da resposta
+          const tipoExtraido = response.data;
+
+          // Atualizar o estado com o atributo extraído
+          setTipo(tipoExtraido);
+      } catch (error) {
+          console.error('Erro ao carregar atributo:', error);
+      }
+  };
+
+  carregarAtributo();
+  },[]);
+
   if(localStorage.getItem('token')){
-    return (
-      <>
-      <Navbar></Navbar>
-      
-          <div class="row">
-      <Sidebar></Sidebar>
-      <NovoUsuarioForm></NovoUsuarioForm>
-      </div>
-      </>
-    );
+    if(tipo=="Administrador"){
+      return (
+        <>
+        <Navbar></Navbar>
+        
+            <div class="row">
+        <Sidebar></Sidebar>
+        <NovoUsuarioForm></NovoUsuarioForm>
+        </div>
+        </>
+      );
+    }
+    else{
+      return (
+        <>
+          <Error></Error>
+        </>
+      );
+    }
   }else{
     return (
       <>
--- src/views/ListarUsuario/ListarUsuario.js
+++ src/views/ListarUsuario/ListarUsuario.js
@@ -2,21 +2,54 @@ import Navbar from "../../components/Dashboard/Navbar/Navbar";
 import Sidebar from "../../components/Dashboard/Sidebar/Sidebar";
 import ListarUsuarioTable from "../../components/Listar/ListarUsuarioTable/ListarUsuarioTable";
 import Error from "../../components/Error/Error"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function ListarUsuario() {
 
-  if(localStorage.getItem('token')){
+  const [tipo, setTipo] = useState('')
 
-    return (
-      <>
-      <Navbar></Navbar>
-      
-      <div class="row ">
-      <Sidebar></Sidebar>
-      <ListarUsuarioTable></ListarUsuarioTable>
-      </div>
-      </>
-    );
+  useEffect(()=>{
+    const token = localStorage.getItem('token')
+    
+    const carregarAtributo = async () => {
+      try {
+
+          const response = await axios.get(`http://localhost:8000/tipo-usuario/${token}`);
+
+          // Extrair o atributo da resposta
+          const tipoExtraido = response.data;
+
+          // Atualizar o estado com o atributo extraído
+          setTipo(tipoExtraido);
+      } catch (error) {
+          console.error('Erro ao carregar atributo:', error);
+      }
+  };
+
+  carregarAtributo();
+  },[]);
+
+
+  if(localStorage.getItem('token')   ){
+    if(tipo=="Administrador"){
+      return (
+        <>
+        <Navbar></Navbar>
+        
+        <div class="row ">
+        <Sidebar></Sidebar>
+        <ListarUsuarioTable></ListarUsuarioTable>
+        </div>
+        </>
+      );
+    }else{
+      return (
+        <>
+          <Error></Error>
+        </>
+      );
+    }
 
   }else{
     return (
